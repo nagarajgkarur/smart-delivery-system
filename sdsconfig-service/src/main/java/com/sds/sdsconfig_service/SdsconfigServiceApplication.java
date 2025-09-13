592fe501c4bc82ecfa69bc9de21c13f825662e85
Nagaraj <nagaraj.g.karur@gmail.com>
2025-09-13 08:43:35 +0530

    created config service

---
+++ sdsconfig-service/src/main/java/com/sds/sdsconfig_service/SdsconfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sds.sdsconfig_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsconfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsconfigServiceApplication.class, args);
+	}
+
+}
---
+++ sdsconfig-service/src/test/java/com/sds/sdsconfig_service/SdsconfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.sdsconfig_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsconfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
